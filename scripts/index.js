3cb28b402560844b06e063943f814599c6c0a7e0
Ibrahim Alausa <tosinibrahim96@gmail.com>
2020-01-05 13:13:19 +0100

    - styling of sections and adding skills modal

--- scripts/index.js
+++ scripts/index.js
@@ -2,11 +2,12 @@
   const execute = () => {
     /* Every time the window is scrolled ... */
     window.addEventListener("scroll", function() {
-      const hiddenElements = document.querySelectorAll(".hideme");
+      const hiddenElements = document.querySelectorAll(".info__box--hide");
       hiddenElements.forEach(elem => {
         const bottom_of_object = elem.offsetTop + elem.offsetHeight;
         const bottom_of_window = window.pageYOffset + window.innerHeight;
 
+        console.log(bottom_of_window, bottom_of_object);
         /* If the object is completely visible in the window, increase opacity with transition */
         if (bottom_of_window > bottom_of_object) {
           elem.style.opacity = 1;
@@ -15,6 +16,24 @@
         }
       });
     });
+
+    const skillsModal = document.getElementById("skillsModal");
+    const skillsmodalTrigger = document.getElementById("skills-modal__trigger");
+    const span = document.querySelector(".close");
+
+    skillsmodalTrigger.onclick = function(event) {
+      event.preventDefault();
+      skillsModal.style.display = "block";
+    };
+    span.addEventListener("click", function() {
+      skillsModal.style.display = "none";
+    });
+    window.addEventListener("click", function(event) {
+      if (event.target == skillsModal) {
+        skillsModal.style.display = "none";
+      }
+    });
   };
+
   execute();
 })();
