f44d93f21fb76667009bbd57cd78588f5cf3a49e
Ibrahim Alausa <tosinibrahim96@gmail.com>
2020-01-10 12:46:35 +0100

    - add appear on scroll from kevin powell

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,30 @@
 (() => {
   const execute = () => {
-    /* Every time the window is scrolled ... */
-    // window.addEventListener("scroll", function() {
-    //   const hiddenElements = document.querySelectorAll(".info__box--hide");
-    //   hiddenElements.forEach(elem => {
-    //     const bottom_of_object = elem.offsetTop + elem.offsetHeight;
-    //     const bottom_of_window = window.pageYOffset + window.innerHeight;
-
-    //     console.log(bottom_of_window, bottom_of_object);
-    //     /* If the object is completely visible in the window, increase opacity with transition */
-    //     if (bottom_of_window > bottom_of_object) {
-    //       elem.style.opacity = 1;
-    //       elem.style.transition =
-    //         "opacity 2s cubic-bezier(0.47, 0, 0.745, 0.715)";
-    //     }
-    //   });
-    // });
-
-    const skillsModal = document.getElementById("skillsModal");
-    const skillsmodalTrigger = document.getElementById("skills-modal__trigger");
-    const span = document.querySelector(".modal__content__close");
-
-    skillsmodalTrigger.onclick = function(event) {
-      event.preventDefault();
-      skillsModal.style.display = "block";
+    const appearOptions = {
+      threshold: 0,
+      rootMargin: "0px 0px -250px 0px"
     };
-    span.addEventListener("click", function() {
-      skillsModal.style.display = "none";
-    });
-    window.addEventListener("click", function(event) {
-      if (event.target == skillsModal) {
-        skillsModal.style.display = "none";
-      }
+
+    const appearOnScroll = new IntersectionObserver(function(
+      entries,
+      appearOnScroll
+    ) {
+      entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+          return;
+        } else {
+          entry.target.style.opacity = 1;
+          entry.target.style.transition =
+            "opacity .9s cubic-bezier(0.47, 0, 0.745, 0.715)";
+          appearOnScroll.unobserve(entry.target);
+        }
+      });
+    },
+    appearOptions);
+
+    const faders = document.querySelectorAll(".info__box--hide");
+    faders.forEach(fader => {
+      appearOnScroll.observe(fader);
     });
   };
 
